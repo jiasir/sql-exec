23c9274c83caab8e4c6c8ac91f558c3e99f59a3e
jiasir <jiasir@icloud.com>
2014-10-14 13:25:52 +0800

    Add Django modules

--- sql-exec.py
+++ sql-exec.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 __author__ = 'Taio'
 
+import os
+import sys
+import logging
+import json
+from django.template import Template, Context
+from django.conf import settings
+import ConfigParser
+
+config_file = ConfigParser.RawConfigParser(allow_no_value=True)
+config_file.read('sql-exec.conf')
+
+settings.configure()
+
+
 
 
 
